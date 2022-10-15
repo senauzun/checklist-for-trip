2920c41adb7701aa5dc2ad77a527e0c4f13b50c6
senauzun <seenauzun@gmail.com>
2022-10-15 21:39:08 +0300

    Initial Commit

---
+++ checklist-for-trip/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  checklist-for-trip
+//
+//  Created by Sena Uzun on 15.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
